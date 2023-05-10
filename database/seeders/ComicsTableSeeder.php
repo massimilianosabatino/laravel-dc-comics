b749eee26294fe62e1117967a317f3cfc9c93770
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-10 17:59:08 +0200

    add: seeder for data from file

---
+++ database/seeders/ComicsTableSeeder.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Comic;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class ComicsTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Get data from comics.php in config folder
+        $comicsArray = config('comics');
+        $newComic = new Comic();
+
+        foreach($comicsArray as $comic){
+            $newComic = new Comic();
+
+            $newComic->title = $comic['title'];
+            $newComic->description = $comic['description'];
+            $newComic->thumb = $comic['thumb'];
+
+            //Remove $ sign from string
+            $price = ltrim($comic['price'], '$');
+            $newComic->price = $price;
+
+            $newComic->series = $comic['series'];
+            $newComic->sale_date = $comic['sale_date'];
+            $newComic->type = $comic['type'];
+
+            //Conver array to string
+            $artists = implode(', ',$comic['artists']);
+            $newComic->artists = $artists;
+            
+            $writers = implode(', ',$comic['writers']);
+            $newComic->writers = $writers;
+
+            $newComic->save();
+        }
+        
+    }
+}
