934f97d15ee7b4b9363da096a800a995726b8601
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-10 18:51:31 +0200

    change: show comics in table

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -1,11 +1,34 @@
 @extends('layout.app')
 
 @section('page.main')
-    
-    <ul>
-        @foreach ($comics as $comic)
-            <li>{{ $comic->title }}</li>
-        @endforeach
-    </ul>
-    
+    <div class="container">
+        {{-- Show comics list --}}
+        <table id="comics-list" class="table">
+            <thead>
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Cover</th>
+                    <th scope="col">Title</th>
+                    <th scope="col">Series</th>
+                    <th scope="col">Type</th>
+                    <th scope="col">Price</th>
+                    <th scope="col">Sale date</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($comics as $comic)
+                <tr>
+                    <th scope="row">{{ $comic->id }}</th>
+                    <td><img class="img-fluid" src="{{ $comic->thumb }}" alt="{{ $comic->title }}"></td>
+                    <td>{{ $comic->title }}</td>
+                    <td>{{ $comic->series }}</td>
+                    <td>{{ $comic->type }}</td>
+                    <td>{{ $comic->price }}</td>
+                    <td>{{ $comic->sale_date }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+        {{-- /Show comics list --}}
+    </div>
 @endsection
