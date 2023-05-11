ddfe90f13c63d8b128d6124e40e2b01ad052bb71
massimilianosabatino <massimiliano.sabatino@gmail.com>
2023-05-11 17:32:59 +0200

    feat: modal windows to confirm delete

--- resources/views/comic/index.blade.php
+++ resources/views/comic/index.blade.php
@@ -42,11 +42,35 @@
                             <td>{{ $comic->sale_date }}</td>
                             <td><a href="{{ route('comics.show', $comic->id) }}" class="btn btn-light">Details</a></td>
                             <td>
-                                <form action="{{ route('comics.destroy', $comic->id) }}" method="post">
-                                    @csrf
-                                    @method('DELETE')
-                                    <button type="submit" class="btn btn-danger">Delete</button>
-                                </form>
+                                {{-- Modal confim --}}
+                                <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modal-{{ $comic->id }}">
+                                    Delete
+                                </button>
+                                {{-- /Modal confim --}}
+                                {{-- Modal content --}}
+                                <div class="modal fade" id="modal-{{ $comic->id }}" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                                    <div class="modal-dialog modal-dialog-centered">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h5 class="modal-title" id="exampleModalLabel">Warning</h5>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                Be careful, this operation is irreversible!
+                                                Do you really want to delete all <strong>{{ $comic->title }}</strong> content?
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                                                <form action="{{ route('comics.destroy', $comic->id) }}" method="post">
+                                                    @csrf
+                                                    @method('DELETE')
+                                                    <button type="submit" class="btn btn-danger">Delete</button>
+                                                </form>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {{-- /Modal content --}}
                             </td>
                         </tr>
                         @endforeach
